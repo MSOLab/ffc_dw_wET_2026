4ddbfda4f85b9289860e31fdb790c17b6182906a
JuneTech <junetech0@gmail.com>
2026-04-27 02:58:23 +0900

    20260427T025803_513725 run setting
    computer: mso02

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from ffc_ddw_sum_et.orchestration import (
     FFcDDWSingleInstanceRunner,
 )
 
-CONFIG_PATH = Path("metadata/20260426/neh_cp_config_14.yaml")
+CONFIG_PATH = Path("metadata/20260426/20260426_config.yaml")
 
 
 def _setup_main_logger(output_dir: Path) -> logging.Logger:
