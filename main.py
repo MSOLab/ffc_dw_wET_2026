33cce02831353db4584b5df385b4a3cff19cf5ec
JuneTech <junetech0@gmail.com>
2026-05-03 22:18:17 +0900

    20260503T215803_006004 run setting
    computer: mso02

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from ffc_ddw_sum_et.orchestration import (
     restore_layout_from_run_dir,
 )
 
-CONFIG_PATH = Path("metadata/20260503/mcf_lb_init_27_config.yaml")
+CONFIG_PATH = Path("metadata/20260503/mcf_lb_init_28_config.yaml")
 
 
 def _parse_args() -> argparse.Namespace:
