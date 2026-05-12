6f2edfb239a52c60e24231f7ac8f6e6bb34fb78f
JuneTech <junetech0@gmail.com>
2026-05-12 10:49:27 +0900

    20260512T101023_662493 run setting
    computer: mso02
    
    - calc_mcf_lb_and_derive_full_sch -> run_flip_makespan_cp_from_incumbent

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from ffc_ddw_sum_et.orchestration import (
     restore_layout_from_run_dir,
 )
 
-CONFIG_PATH = Path("metadata/20260511/mcf_lb_neh_cp_ipwcp_base_cpsat_2_config.yaml")
+CONFIG_PATH = Path("metadata/20260512/mcf_lb_flip_makespan.yaml")
 
 
 def _parse_args() -> argparse.Namespace:
