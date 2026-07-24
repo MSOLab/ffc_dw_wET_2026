676fa82348d06a3af1eb184e86fce1e665540d49
JuneTech <junetech0@gmail.com>
2026-07-24 14:20:12 +0900

    20260724_active_but_last_semi_csr_ab/20260724T135036_009404 run setting
    computer: calop4

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from ffc_ddw_sum_et.orchestration import (
     restore_layout_from_run_dir,
 )
 
-CONFIG_PATH = Path("metadata/20260724/csr_k_f_cumulative_recon_ab.yaml")
+CONFIG_PATH = Path("metadata/20260724/csr_coarsen_mode_active.yaml")
 
 
 def _parse_args() -> argparse.Namespace:
